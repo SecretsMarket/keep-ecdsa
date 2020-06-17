fefc2a6b2474b3a1916172c39bd66b698c680153
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-06-17 17:14:48 +0200

    ETH staking contract
    The contract is used to delegate stake in ETH to the specific operator
    with authorizer and beneficiary.

---
+++ solidity/contracts/ETHStaking.sol
@@ -0,0 +1,51 @@
+/**
+▓▓▌ ▓▓ ▐▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄
+▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▌▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  ▓▓▓▓▓▓    ▓▓▓▓▓▓▓▀    ▐▓▓▓▓▓▓    ▐▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+  ▓▓▓▓▓▓▄▄▓▓▓▓▓▓▓▀      ▐▓▓▓▓▓▓▄▄▄▄         ▓▓▓▓▓▓▄▄▄▄         ▐▓▓▓▓▓▌   ▐▓▓▓▓▓▓
+  ▓▓▓▓▓▓▓▓▓▓▓▓▓▀        ▐▓▓▓▓▓▓▓▓▓▓         ▓▓▓▓▓▓▓▓▓▓▌        ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  ▓▓▓▓▓▓▀▀▓▓▓▓▓▓▄       ▐▓▓▓▓▓▓▀▀▀▀         ▓▓▓▓▓▓▀▀▀▀         ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▀
+  ▓▓▓▓▓▓   ▀▓▓▓▓▓▓▄     ▐▓▓▓▓▓▓     ▓▓▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▐▓▓▓▓▓▌
+▓▓▓▓▓▓▓▓▓▓ █▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓ ▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓
+
+                           Trust math, not hardware.
+*/
+
+pragma solidity 0.5.17;
+
+import "@keep-network/keep-core/contracts/KeepRegistry.sol";
+import "@keep-network/keep-core/contracts/KeepStaking.sol";
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+/// @title ETH Staking
+/// @notice A staking contract for ETH staking. An owner of the ETH can delegate
+/// ETH as a stake to an operator. The value of ETH the owner is willing to stake
+/// should be deposited in `ETHBonding` contract for the given operator.
+contract ETHStaking is KeepStaking {
+    constructor(address keepRegistryAddress)
+        public
+        KeepStaking(keepRegistryAddress)
+    {}
+
+    event Staked(
+        address indexed owner,
+        address indexed beneficiary,
+        address indexed authorizer
+    );
+
+    function stake(
+        address operator,
+        address payable beneficiary,
+        address authorizer
+    ) external payable {
+        address _from = msg.sender;
+
+        operators[operator] = Operator(0, _from, beneficiary, authorizer);
+        ownerOperators[_from].push(operator);
+
+        emit Staked(_from, beneficiary, authorizer);
+    }
+}
